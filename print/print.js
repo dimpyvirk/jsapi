7756397a664f5980d955e39a475a81e4fb1afeeb
Stephen Lead <stephen.lead@gmail.com>
2013-01-18 16:13:49 +1100

    added print task sample

--- print/print.js
+++ print/print.js
@@ -3,9 +3,10 @@ dojo.require("dijit.layout.ContentPane");
 dojo.require("dijit.form.Button");
 dojo.require("esri.map");
 dojo.require("esri.dijit.Print");
+
+var printer;
       
 function init() {
-//esri.config.defaults.io.proxyUrl = "/arcgisserver/apis/javascript/proxy/proxy.ashx";
 
 	var map = new esri.Map("map", {
 	  center: [-90.733, 30.541],
@@ -13,11 +14,39 @@ function init() {
 	});
 	
 	// print dijit
-	var printer = new esri.dijit.Print({
+	
+	var printTemplates = [
+		{
+		    label: "Map",
+		    format: "PDF",
+		    layout: "MAP_ONLY",
+		    exportOptions: {
+		      width: 500,
+		      height: 400,
+		      dpi: 96
+		    }
+		}, {
+		    label: "Layout",
+		    format: "PDF",
+		    layout: "A4 Portrait",
+		    layoutOptions: {
+		      titleText: "My Print",
+		      authorText: "esri",
+		      copyrightText: "My Company",
+		      scalebarUnit: "Miles",
+		    }
+		}];
+		
+	printer = new esri.dijit.Print({
 	  map: map,
+	  templates: printTemplates,
 	  url: "http://services.azuron.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task"
 	}, dojo.byId("printButton"));
+	
 	printer.startup();
+	dojo.connect(printer,'onError',function(error){
+	    alert('Print error : ' + error);
+	  });
 	
 	var url = "http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer";
 	var tiledLayer = new esri.layers.ArcGISTiledMapServiceLayer(url, { "id": "Ocean" });

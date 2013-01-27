b05ae0ea4f43f78204a3f7275eed2349b8f50976
Stephen Lead <stephen.lead@gmail.com>
2013-01-28 07:54:30 +1100

    custom zoom slider specified in map constructor

---
+++ backbone/js/basemapModel.js
@@ -0,0 +1,12 @@
+//Namespace
+if (!this.fe || typeof this.fe !== 'object') {
+	this.fe = {};
+} 
+
+/* Map Model */
+fe.basemapModel = Backbone.Model.extend({
+    initialize: function (args) {
+    	var userConfig = args.userConfig;
+    
+    }
+});
--- backbone/js/mapModel.js
+++ backbone/js/mapModel.js
@@ -1,14 +1,19 @@
-dojo.require('esri.map');
-
 //Namespace
 if (!this.fe || typeof this.fe !== 'object') {
 	this.fe = {};
-} 
+}
+
+//Load the required DOJO libraries
+dojo.require('esri.map');
+dojo.require("esri.dijit.Popup");
 
 /* Map Model */
 fe.MapModel = Backbone.Model.extend({
-    initialize: function () {
-    	var userConfig = this.attributes.userConfig;
+    initialize: function (args) {
+    	var userConfig = args.userConfig;
+    	
+    	//Define some arrays and counters, used to keep track of layers, extents, etc
+    	this.set("basemapArray", {});
     	
     	//Retrieve any parameters specified in the URL (eg extent, basemap, layers, etc)
     	var sXmin = parseFloat(this.URLLookup("xmin"));
@@ -18,6 +23,7 @@ fe.MapModel = Backbone.Model.extend({
     	var sLat = parseFloat(this.URLLookup("lat"));
     	var sLong = parseFloat(this.URLLookup("long"));
     	var sBuffer = parseFloat(this.URLLookup("buffer"));
+    	var spatRef = userConfig.application.extent.spatialRef || 102113;
     	
     	// If the start extent has been provided in the URL, as the bounding box in Web Mercator
     	var startExtent;
@@ -27,8 +33,9 @@ fe.MapModel = Backbone.Model.extend({
     			ymin: sYmin,
     			xmax: sXmax,
     			ymax: sYmax,
-    			spatialReference: { 'wkid': userConfig.application.extent.spatialRef}
+    			spatialReference: { 'wkid': spatRef}
     		});
+    	// The lat/long and (optionally) the buffer amount
     	} else if (sLat && sLong) {
     		if(!sBuffer) {
     			//If scale is not provided in the URL, use the application default, or a hard default
@@ -41,7 +48,7 @@ fe.MapModel = Backbone.Model.extend({
     			ymin: wmPoint.y - sBuffer,
     			xmax: wmPoint.x + sBuffer,
     			ymax: wmPoint.y + sBuffer,
-    			spatialReference: { 'wkid': wmPoint.spatialRef}
+    			spatialReference: { 'wkid': spatRef}
     		});
     		
     	} else {
@@ -51,18 +58,59 @@ fe.MapModel = Backbone.Model.extend({
 	        	ymin: userConfig.application.extent.yMin,
 	        	xmax: userConfig.application.extent.xMax,
 	        	ymax :userConfig.application.extent.yMax,
-	        	spatialReference: { 'wkid': userConfig.application.extent.spatialRef}
+	        	spatialReference: { 'wkid': spatRef}
 	        });
 	    }
+	    
+	    //Create a customised map popup
+	    var popup = this.createPopup();
         
-        this.map = new esri.Map(this.attributes.div, { extent: startExtent });
+        //Build the map object
+        var sliderStyle = args.sliderStyle || "default";
+        var isZoomSlider = true;
+        if (args.isZoomSlider != undefined) {
+        	isZoomSlider = args.isZoomSlider;
+        	if(!isZoomSlider) {
+        		//Needed due to a bug at v3.3 whereby the slider is still shown unless the large option is chosen
+        		sliderStyle = "large";
+        	} 
+        }
         
+        this.map = new esri.Map(args.div, {
+        	extent: startExtent,
+        	popup: popup,
+        	sliderStyle: sliderStyle,
+        	isZoomSlider: isZoomSlider,
+        	wrapAround180: true
+        });
+        
+        //TODO: replace this with the proper basemaps
         var tiledMapServiceLayer = new esri.layers.ArcGISTiledMapServiceLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer');
         this.map.addLayer(tiledMapServiceLayer);
-
-        dojo.connect(this.map, 'onExtentChange', dojo.hitch(this, this.updateExtent));
+        
+        //Set listeners for various map events
+        if(args.feZoomSlider) {
+        	//Override the default zoom slider with a custom slider
+	        dojo.connect(this.map, 'onLoad', dojo.hitch(this, this.createCustomSlider));
+	    }
+        dojo.connect(this.map, 'onExtentChange', dojo.hitch(this, this.extentChanged));
+        
     },
-    updateExtent: function () {
+    createPopup: function() {
+	    var mapPopup = new esri.dijit.Popup({
+	    	fillSymbol: new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([255,0,0]), 2), new dojo.Color([255,255,0,0.25])),
+	    	lineSymbol: new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_DASH,
+	    	  new dojo.Color([255,0,0]), 3),
+	    	markerSymbol: new esri.symbol.SimpleMarkerSymbol('circle', 32, null, new dojo.Color([0, 0, 0, 0.25])),
+	    	//marginLeft: 10,
+	    	//marginTop: 10,
+	    	//offsetX:3,
+	    	//offsetY:3,
+	    	zoomFactor:4
+	    }, dojo.create("div"));
+	    return mapPopup;
+    },
+    extentChanged: function () {
         var extent = {
             xmin: this.map.extent.xmin.toFixed(0),
             xmax: this.map.extent.xmax.toFixed(0),
@@ -70,8 +118,10 @@ fe.MapModel = Backbone.Model.extend({
             ymax: this.map.extent.ymax.toFixed(0)
         };
         this.set(extent);
+        
     },
     setValuesFromURL: function (){
+    	//TODO: interpret these URL-derived values and do something with them
     	this.set("locateString",this.URLLookup("adr"));
     	this.set("baseMap", decodeURIComponent(this.URLLookup("bm")));
     	this.set("isEmbedded",this.URLLookup("embed"));
@@ -89,5 +139,34 @@ fe.MapModel = Backbone.Model.extend({
     	var results = (new RegExp("[\\?&]"+name+"=([^&#]*)")).exec(window.location.href);
     	if ( results == null ) {return null}
     	else {return results[1]}
+    },
+    createCustomSlider: function () {
+    	jQuery('#zoomSliderDiv').html('<div id="zoomSliderPlus" title="Zoom in"></div><div id="zoomSliderCustom" title="Drag to zoom"></div><div id="zoomSliderMinus" title="Zoom out"></div>');
+    	this.map.customMapSlider = jQuery("#zoomSliderCustom").slider({
+    		min: 0,
+    		max: this.map._params.lods.length - 1,
+    		value: this.map.getLevel(),
+    		orientation: "vertical",
+    		range: "min",
+    		change: function(event, ui) {
+    			if (this.map.getLevel() !== ui.value) {
+    				this.map.setLevel(ui.value);
+    			}
+    		}
+    	});
+    
+    	/*------------------------------------*/
+    	// ZOOM SLIDER BUTTONS
+    	/*------------------------------------*/
+    	jQuery(document).on('click','#zoomSliderMinus',function(event) {
+    		var currentValue = this.map.customMapSlider.slider("option", "value");
+    		this.map.customMapSlider.slider("option", "value", currentValue - 1);
+        });
+    	jQuery(document).on('click','#zoomSliderPlus',function(event) {
+    		var currentValue = this.map.customMapSlider.slider("option", "value");
+    		this.map.customMapSlider.slider("option", "value", currentValue + 1);
+        });
+    	// SHOW ZOOM SLIDER
+    	jQuery('#zoomSliderDiv').show();
     }
 });
